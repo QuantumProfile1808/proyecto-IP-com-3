f3c3b501e46d29c1cc0fcd973d1abb02c328fed9
Uri <85770520+yuriman007@users.noreply.github.com>
2025-11-17 17:35:53 -0300

    cambios en sort_selection

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -17,12 +17,43 @@ def init(vals):
     fase = "buscar"
 
 def step():
-    # TODO:
-    # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-    #   Al terminar el barrido, pasar a fase "swap".
-    # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
-    #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    #
-    # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
+    global items, n, i, j, min_idx, fase
+
+    if i >= n - 1:    #cuando i llega al final no ordenado, se termina
+        return {"done": True} 
+
+    # ---- FASE BUSCAR ----
+    if fase == "buscar":
+        # Mientras j esté dentro de la lista seguimos comparando
+        if j < n:
+            a = min_idx
+            b = j       # índice del elemento que estamos comparando ahora
+            
+            if items[j] < items[min_idx]: # actualizar mínimo si corresponde
+                min_idx = j
+
+            j += 1   
+            return {"a": a, "b": b, "swap": False, "done": False}
+
+        # terminó el barrido → pasar a fase swap
+        fase = "swap"
+        return {"a": min_idx, "b": i, "swap": False, "done": False}
+
+    # ---- FASE SWAP ----
+    if fase == "swap":
+        if min_idx != i:   # Si el mínimo encontrado no está donde debería, lo intercambiamos
+            
+            items[i], items[min_idx] = items[min_idx], items[i]
+            a = i
+            b = min_idx
+        else:    # Si min_idx == i, no hay cambio: el mínimo ya está en su lugar
+            a = i
+            b = i
+
+        # mover a la próxima iteración
+        i += 1
+        j = i + 1
+        min_idx = i
+        fase = "buscar"
+
+        return {"a": a, "b": b, "swap": (a != b), "done": False}
